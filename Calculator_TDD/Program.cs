35fc2a6d43f2f1597e4b83322def44d86beccc11
Sohrab Azar <sohrabsa@gmail.com>
2021-01-29 00:17:30 +0100

    Add project files.

---
+++ Calculator_TDD/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Calculator_TDD
+{
+    public class Program
+    {
+        public static InputType lastInputType = InputType.Operation;
+
+        static void Main(string[] args)
+        {
+            ShowConsolePromot(lastInputType);
+
+            //Get user input
+            string userInput = Console.ReadLine();
+
+            switch (lastInputType)
+            {
+                case InputType.Number:
+                    /// entering operation
+                    // check if input is valid, if yes then move forward if not ask to enter again
+                    // Set opration type based on what sign was entered
+                    break;
+                case InputType.Operation:
+                    /// entering number
+                    // check if input is valid, if yes then move forward if not ask to enter again
+                    // if valid
+                    // if first input then set result to it
+                    // else based on what operation type that was entered is do operation on result
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #region MEMBERS
+        public enum InputType
+        {
+            Number,
+            Operation,
+        }
+
+        enum OperationType
+        {
+            none,
+            add,
+            reduce,
+            multiple,
+            devide,
+            convertCelsiusToFarenhit,
+            convertFarenhitToCelsius
+        }
+
+        enum SpecialCommand
+        {
+            quit,
+            help,
+            list,
+            reset
+        }
+        #endregion
+
+        #region METHODS
+        private static void ShowConsolePromot(InputType lastInputType)
+        {
+            switch (lastInputType)
+            {
+                case InputType.Number:
+                    Console.Write("OPERATION >");
+                    break;
+                case InputType.Operation:
+                    Console.Write("NUMBER >");
+                    break;
+            }
+        }
+        #endregion
+
+    }
+}
