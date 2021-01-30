1a8d79b437dbf829c465fcdf48778fcb857068da
Sohrab Azar <sohrabsa@gmail.com>
2021-01-30 17:15:34 +0100

    moved is input valid test to class library

--- CalculatorClassLibraryTests/InputValidatorTests.cs
+++ CalculatorClassLibraryTests/InputValidatorTests.cs
@@ -77,6 +77,45 @@ namespace CalculatorClassLibrary.Tests
             Assert.AreEqual(excpeted_2, actual_2);
         }
 
+        [TestMethod()]
+        public void IsUserInputValidTest()
+        {
+            // TEST CASE 1
+            // Arrange
+            bool excpeted_1 = true;
+            // Act
+            Core.previousInputType = Enumrations.InputType.Number;
+            bool actual_1 = InputValidator.IsUserInputValidMath("+");
+            // Assert
+            Assert.AreEqual(excpeted_1, actual_1);
+
+            // TEST CASE 2
+            // Arrange
+            bool excpeted_2 = false;
+            // Act
+            Core.previousInputType = Enumrations.InputType.Number;
+            bool actual_2 = InputValidator.IsUserInputValidMath("as");
+            // Assert
+            Assert.AreEqual(excpeted_2, actual_2);
+
+            // TEST CASE 3
+            // Arrange
+            bool excpeted_3 = true;
+            // Act
+            Core.previousInputType = Enumrations.InputType.Operation;
+            bool actual_3 = InputValidator.IsUserInputValidMath("123");
+            // Assert
+            Assert.AreEqual(excpeted_3, actual_3);
+
+            // TEST CASE 4
+            // Arrange
+            bool excpeted_4 = false;
+            // Act
+            Core.previousInputType = Enumrations.InputType.Operation;
+            bool actual_4 = InputValidator.IsUserInputValidMath("asd");
+            // Assert
+            Assert.AreEqual(excpeted_4, actual_4);
+        }
     }
 
 
--- Calculator_TDDTests/ProgramTests.cs
+++ Calculator_TDDTests/ProgramTests.cs
@@ -10,47 +10,6 @@ namespace Calculator_TDD.Tests
     public class ProgramTests
     {
 
-
-        //[TestMethod()]
-        //public void IsUserInputValidTest()
-        //{
-        //    // TEST CASE 1
-        //    // Arrange
-        //    bool excpeted_1 = true;
-        //    // Act
-        //    Program.previousInputType = Program.InputType.Number;
-        //    bool actual_1 = Program.IsUserInputValidMath("+");
-        //    // Assert
-        //    Assert.AreEqual(excpeted_1, actual_1);
-
-        //    // TEST CASE 2
-        //    // Arrange
-        //    bool excpeted_2 = false;
-        //    // Act
-        //    Program.previousInputType = Program.InputType.Number;
-        //    bool actual_2 = Program.IsUserInputValidMath("as");
-        //    // Assert
-        //    Assert.AreEqual(excpeted_2, actual_2);
-
-        //    // TEST CASE 3
-        //    // Arrange
-        //    bool excpeted_3 = true;
-        //    // Act
-        //    Program.previousInputType = Program.InputType.Operation;
-        //    bool actual_3 = Program.IsUserInputValidMath("123");
-        //    // Assert
-        //    Assert.AreEqual(excpeted_3, actual_3);
-
-        //    // TEST CASE 4
-        //    // Arrange
-        //    bool excpeted_4 = false;
-        //    // Act
-        //    Program.previousInputType = Program.InputType.Operation;
-        //    bool actual_4 = Program.IsUserInputValidMath("asd");
-        //    // Assert
-        //    Assert.AreEqual(excpeted_4, actual_4);
-        //}
-
         [TestMethod()]
         public void CalculateResultTest_MathOperations()
         {
